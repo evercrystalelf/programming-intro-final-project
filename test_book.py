1d0c7dbc304bd7084904133f5e2247aafdd88bf0
tere646 <72846467+tere646@users.noreply.github.com>
2020-11-07 15:14:19 -0700

    Update test_book.py

--- test_book.py
+++ test_book.py
@@ -1,30 +1,17 @@
-import csv
 import random
+import csv
 
-middleman = [] # opening any empty list to put in all the adult patients
+def _makeIDGen():
+    i = 1
+    while True:
+        yield i
+        i += 1
 
-with open("patients.csv", 'r') as csvfile: 
-    
-    csvreader = csv.reader(csvfile) # making a reader object
-    
-    for row in csvreader:  # adding each line as an element in middleman
-        middleman.append(row)  
-        
-middleman.pop(0) #removing the header row
-
-cardio_case=[] #opening an empty list to put the adult patients in
-
-
-for i in range(300): # all lists in list
-    if int(middleman[i][2]) >= 12: # if index 2 of each list is adult
-        cardio_case.append(middleman[i]) # add the list to cardio_case
-        
-_disease_at_admission = ['stroke', 'hernia', 'macular degeneration', 'tuberculosis', 'colitis']
-
-_comorbidities = ['COPD', 'cancer', 'diabetes', 'osteoarthritis', 'depression']
+_idGen = _makeIDGen()
 
 class Adult:
     def __init__(self):
+        self.id =  next(_idGen)
         self.triglycerides = random.randint(35,200)
         self.HDL = random.randint(20,100)
         self.LDL = random.randint(40,190)
@@ -34,24 +21,107 @@ class Adult:
         self.comorbidities = random.choice(_comorbidities)
 
     def __str__(self):
-        return f'{self.triglycerides}, {self.HDL}, {self.LDL}, {self.total_cholesterol}, {self.CRP}, {self.disease}, {self.comorbidities}'
+        return f'ID: {self.id}, Triglycerides: {self.triglycerides}, HDL: {self.HDL}, LDL: {self.LDL}, Total Cholesterol: {self.total_cholesterol}, CRP: {self.CRP}, Disease at Admission: {self.disease}, Comorbidity: {self.comorbidities}'
     
     def __repr__(self):
         return str(self)
     
-for i in range(len(cardio_case)):
-    case = Adult()
-    cardio_case[i].append(case)    
+adults = []
 
-with open('transform.csv', 'w', newline='') as file:
+for i in range(0, 300):
+    a = Adult()
+    adults.append(a)    
+    
+with open('total.csv', 'w', newline='') as file:
+    # writer for writing the csv rows in the file
     w = csv.writer(file)
-    w.writerow(cardio_case)
+    # use writerow to write the header in csv file
+    w.writerow(['id', 'Triglycerides', 'HDL', 'LDL', 'Total Cholesterol', 'CRP', 'Disease at Admission', 'Comobiditiy'])
+    # write every patinet to the csv file
+    for a in adults:
+        w.writerow([a.id, a.triglycerides, a.HDL, a.LDL, a.total_cholesterol, a.CRP, a.disease, a.comorbidities])  
+        
+import pandas as pd
 
-cardio_casecont = [] # opening an intermediate list
+p = pd.read_csv("patients.csv")
 
-with open("transform.csv", 'r') as csvfile: 
+t = pd.read_csv("total.csv")
+
+merged = p.merge(t, on='id')
+
+merged.to_csv("combo.csv", index = False)        
+
+middleman = [] 
+
+with open("combo.csv", 'r') as csvfile: 
+    
     csvreader = csv.reader(csvfile) # making a reader object
-    for row in csvreader:  # adding each line as an element in cardio_casecont
-        cardio_casecont.append(row)  
+    
+    for row in csvreader:  # adding each line as an element in middleman
+        middleman.append(row)  
 
-print(len(cardio_casecont))
+middleman.pop(0) #removing the header row
+
+cardio_case=[] #opening an empty list to put the adult patients in
+
+
+for i in range(300): # all lists in list
+    if int(middleman[i][2]) >= 12: # if index 2 of each list is adult
+        cardio_case.append(middleman[i]) # add the list to cardio_case
+
+def risk_score ():
+    points = 0   #setting itteration value to 0 for addition
+    
+    for f in (len(cardio_case)):                           
+                               
+        if cardio_case[12] > 150: # if the value meets the condition
+            points+=1          # a point is added
+    
+        if cardio_case[3] == M and cardio_case[13] < 40:
+            points+=1
+            
+            
+        if cardio_case[3] == F and cardio_case[13] <50:
+            points+=1
+   
+        if cardio_case[14] > 100:
+            points+=1
+    
+        if cardio_case[15] > 200:
+            points+=1
+    
+        if cardio_case[16] > 2:
+            points+=1
+    
+    risk_score = points # the total points
+    
+    cardio_case = f.cardio_case.append(risk_score) # adding the point total to the adult patients
+
+risk_score()    
+    
+def Risk ():      # assigning each total point possibliity to a risk estimation
+    for item in (len(cardio_case)):
+        if cardio_case[19] == 0:
+            Risk = low
+        if cardio_case[19] == 1:
+            Risk = mild
+        if cardio_case[19] == 2:
+            Risk = moderate
+        if cardio_case[19] == 3:
+            Risk = moderate_high
+        if cardio_case[19] == 4:
+            Risk = high
+        if cardio_case[19] == 5:
+            Risk = major
+    cardio_case = item.cardio_case.append(Risk)
+    
+Risk()    
+    
+print(len(cardio_case))
+
+print(type(cardio_case))
+
+print(cardio_case[0])
+
+
+      
